de43e464f59b86a0e1440b8b028c0045eb126941
dennisvg111 <dennisvg111@gmail.com>
2023-04-28 10:18:10 +0200

    Add documentation to RijndaelSha1Encryption, add tests (#1)

---
+++ DG.Cryptography/DG.Cryptography.Tests/RijndaelSha1EncryptionTests.cs
@@ -0,0 +1,21 @@
+﻿using DG.Cryptography.Encryption;
+using Xunit;
+
+namespace DG.Cryptography.Tests
+{
+    public class RijndaelSha1EncryptionTests
+    {
+        [Theory]
+        [InlineData("test", "key")]
+        public void EncryptionReversible(string plainText, string key)
+        {
+            var encryption = RijndaelSha1Encryption.Default;
+
+            var encrypted = encryption.Encrypt(plainText, key);
+            var decrypted = encryption.Decrypt(encrypted, key);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+    }
+}
